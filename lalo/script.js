483b31255384c052be62e1cf0f27f2083144c9a1
taviya lalji <116023159+taviyalalji@users.noreply.github.com>
2023-09-18 17:39:08 +0530

    Update script.js

--- lalo/script.js
+++ lalo/script.js
@@ -1,102 +1,101 @@
-// function score(number1, number2) {
-//     let x = number1 + number2;
-//     console.log(x)
-// }
-// score(23, 54)
+function score(number1, number2) {
+    let x = number1 + number2;
+    console.log(x)
+}
+score(23, 54)
 
 
-//check data type 
-// let StringValue = "hello world"
-// let NumberValue = 213
-// let BooleanValue = true
-// let BigIntValue = 123235435353n
-// let NullValue = null
-// let objectValue = { name: 'uday', age: 20 }
-// let SymolValue = Symbol("value")
-// let undefindeValue =
-
-//     console.log("String :" + typeof StringValue)
-// console.log("Number :" + typeof NumberValue)
-// console.log("Boolean :" + typeof BooleanValue)
-// console.log("Bigint :" + typeof BigIntValue)
-// console.log("Null :" + typeof NullValue)
-// console.log("object :" + typeof objectValue)
-// console.log("Symbol :" + typeof SymolValue)
-// console.log("undefinde :" + typeof undefindeValue)
+// check data type 
+let StringValue = "hello world"
+let NumberValue = 213
+let BooleanValue = true
+let BigIntValue = 123235435353n
+let NullValue = null
+let objectValue = { name: 'uday', age: 20 }
+let SymolValue = Symbol("value")
+let undefindeValue =
+console.log("String :" + typeof StringValue)
+console.log("Number :" + typeof NumberValue)
+console.log("Boolean :" + typeof BooleanValue)
+console.log("Bigint :" + typeof BigIntValue)
+console.log("Null :" + typeof NullValue)
+console.log("object :" + typeof objectValue)
+console.log("Symbol :" + typeof SymolValue)
+console.log("undefinde :" + typeof undefindeValue)
 
 
 //stack and heap
-//stack (primitive) value
-//heap (non - primitive ) value
+// stack (primitive) value
+// heap (non - primitive ) value
 
 //stack ..return copy value not return original reference 
-// let YoutubeName = "codertube"
-// let FetchName = YoutubeName
+let YoutubeName = "codertube"
+let FetchName = YoutubeName
 // //change value
-// FetchName = "changename"
+FetchName = "changename"
 
-// console.log(YoutubeName)
-// console.log(FetchName)
+console.log(YoutubeName)
+console.log(FetchName)
 
 // //heap ... return original reference not return copy value 
-// let OneUser = {
-//     email: "xyz@gmail.com",
-//     age: 23
-// }
-// let TwoUser = OneUser
+let OneUser = {
+    email: "xyz@gmail.com",
+    age: 23
+}
+let TwoUser = OneUser
 // //change value
-// OneUser.age = 54
+OneUser.age = 54
 
-// console.log(OneUser)
-// console.log(TwoUser)
+console.log(OneUser)
+console.log(TwoUser)
 
 
 //.............string ...............
 
-// let name = "john"
-// let age = 54
+let name = "john"
+let age = 54
 // //short for of concatenate ..use backtisk(``)for beter programing parformance 
-// console.log(`hello guys my name is ${name} and my age is ${age}`)
+console.log(`hello guys my name is ${name} and my age is ${age}`)
 
-// const gameName = new String('computer')
-// //check charecter langth
-// console.log(gameName.length)
+const gameName = new String('computer') 
+//check charecter langth
+console.log(gameName.length)
 // //check position charecter 
-// console.log(gameName.charAt(2))
-// //chack charecter position(key)
-// console.log(gameName.indexOf('r'))
+console.log(gameName.charAt(2))
+//chack charecter position(key)
+console.log(gameName.indexOf('r'))
 
 
-// //url replace 
-// let url = "https://www.google.com/search?q=js&oq=&aqs=chrome.2.69i59i450l8.473507j0j7&sourceid=chrome&ie=UTF-8"
-// console.log(url.replace('&', '20'))
+//url replace 
+let url = "https://www.google.com/search?q=js&oq=&aqs=chrome.2.69i59i450l8.473507j0j7&sourceid=chrome&ie=UTF-8"
+console.log(url.replace('&', '20'))
 
-// //check url charecters 
-// let Vurl = "https://www.google.com/search?q=js&oq=&aqs=chrome.2.69i59i450l8.473507j0j7&sourceid=chrome&ie=UTF-8"
-// console.log(Vurl.includes('onion'))
+//check url charecters 
+let Vurl = "https://www.google.com/search?q=js&oq=&aqs=chrome.2.69i59i450l8.473507j0j7&sourceid=chrome&ie=UTF-8"
+console.log(Vurl.includes('onion'))
 
 
 //...............maths.................
 
 
 
-// //to fixed a value of number ex. 1000.0
-// let price = 1000
-// console.log(price.toFixed(1))
+//to fixed a value of number ex. 1000.0
+let price = 1000
+console.log(price.toFixed(1))
 
 
-// let price = 10000
-// console.log(price.toLocaleString('en-IN'))
+let price = 10000
+console.log(price.toLocaleString('en-IN'))
 
 //check max value and min value
-// let value = [12, 23, 4, 53, 34, 34, 3545, 665, 12, 43656, 865, 534]
-// console.log(Math.min(...value))
-// console.log(Math.max(...value))
+let value = [12, 23, 4, 53, 34, 34, 3545, 665, 12, 43656, 865, 534]
+console.log(Math.min(...value))
+console.log(Math.max(...value))
 
 
 //random number 
-// console.log(Math.random())
-// console.log(Math.floor(Math.random() * 10))
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 10))
 let min = 100
 let max = 1000
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
